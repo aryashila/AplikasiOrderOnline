b0f21887525562d7d035cb07f6721d786c487a8e
aryashila <distiaralandephy@gmail.com>
2016-09-10 11:39:13 +0700

    First Commit Membuat design dari aplikasi

---
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl1031/orderonline/MainActivity.java
@@ -0,0 +1,16 @@
+package id.sch.smktelkom_mlg.tugas01.xiirpl1031.orderonline;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
